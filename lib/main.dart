51b1f7fba88a7470690b846c14877dd7ef96d23d
YeJun, Jung <yejun614@naver.com>
2023-02-21 01:17:23 +0900

    Package: firebase auth and firestore setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,39 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:safe_movement_app/firebase_options.dart';
+
 import 'screen/intro_screen.dart';
 
-void main() {
+void main() async {
+  // Firebase Setup
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Firebase Emulator
+  if (kDebugMode) {
+    print("------ DEBUG MODE ------");
+
+    try {
+      // Firebase Emulators Setup
+      FirebaseFirestore.instance.useFirestoreEmulator('127.0.0.1', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('127.0.0.1', 9099);
+
+      // Make a test user
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: "test@test.com", password: "12345678");
+
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  // Run App
   runApp(const SafeMovementApp());
 }
 
