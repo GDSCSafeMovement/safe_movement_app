3bf2920c4ff22ce86ae9fc5d8e3a5afb049d40ae
YeJun, Jung <yejun614@naver.com>
2023-03-23 01:58:38 +0900

    Feat: add google map widget in movement screen

--- lib/screen/movement_screen.dart
+++ lib/screen/movement_screen.dart
@@ -10,9 +10,10 @@ class MovementScreen extends StatefulWidget {
 }
 
 class _MovementScreenState extends State<MovementScreen> {
+  bool _isActive = false;
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
 
-  // TODO: Get current posittion
+  // TODO: Get current position
   static const CameraPosition _currentPosition = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -20,14 +21,55 @@ class _MovementScreenState extends State<MovementScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _currentPosition,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
+    return Scaffold(
+      bottomNavigationBar: _isActive ? BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.stop),
+            label: 'Stop',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.stop),
+            label: 'Stop',
+          ),
+        ],
+        onTap: (int index) {
+          setState(() {
+            _isActive = false;
+          });
+        }
+      ) : null,
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _currentPosition,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            padding: _isActive ? EdgeInsets.all(0) : EdgeInsets.fromLTRB(0, 0, 0, 50),
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+          ),
+          if (!_isActive)
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+              child: FilledButton(
+                onPressed: () {
+                  setState(() {
+                    _isActive = true;
+                  });
+                },
+                child: Text("안심귀가 시작")
+              ),
+            ),
+          ),
+        ]
+      )
     );
   }
 }
