7f5faf9c28324213bf67300a95573a38f4d38861
YeJun, Jung <yejun614@naver.com>
2023-03-31 06:07:40 +0900

    Fix: bottom button label in movement screen

--- lib/screen/movement_screen.dart
+++ lib/screen/movement_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:get/get.dart';
 
 class MovementScreen extends StatefulWidget {
   const MovementScreen({ super.key });
@@ -10,7 +11,6 @@ class MovementScreen extends StatefulWidget {
 }
 
 class _MovementScreenState extends State<MovementScreen> {
-  bool _isActive = false;
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
 
   // TODO: Get current position
@@ -22,23 +22,6 @@ class _MovementScreenState extends State<MovementScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: _isActive ? BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.stop),
-            label: 'Stop',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.stop),
-            label: 'Stop',
-          ),
-        ],
-        onTap: (int index) {
-          setState(() {
-            _isActive = false;
-          });
-        }
-      ) : null,
       body: Stack(
         children: [
           GoogleMap(
@@ -47,24 +30,26 @@ class _MovementScreenState extends State<MovementScreen> {
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
             },
-            padding: _isActive ? EdgeInsets.all(0) : EdgeInsets.fromLTRB(0, 0, 0, 50),
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 50),
             myLocationEnabled: true,
             myLocationButtonEnabled: true,
           ),
-          if (!_isActive)
           Positioned(
             left: 0,
             right: 0,
             bottom: 0,
             child: Container(
-              padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
               child: FilledButton(
                 onPressed: () {
                   setState(() {
-                    _isActive = true;
+                    Get.defaultDialog(
+                      title: "Find",
+                      content: Container(),
+                    );
                   });
                 },
-                child: Text("안심귀가 시작")
+                child: const Text("Find Children"),
               ),
             ),
           ),
