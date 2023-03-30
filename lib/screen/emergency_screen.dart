8e5b24a4078a397fa92c5809255f85a974c1b472
YeJun, Jung <yejun614@naver.com>
2023-03-31 06:05:53 +0900

    Fix: styles in emergency screen

--- lib/screen/emergency_screen.dart
+++ lib/screen/emergency_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../utility/user.dart';
 
 class EmergencyScreen extends StatefulWidget {
   const EmergencyScreen({ super.key });
@@ -8,20 +11,48 @@ class EmergencyScreen extends StatefulWidget {
 }
 
 class _EmergencyScreenState extends State<EmergencyScreen> {
+  String _emergencyPhone = "";
+
+  void loadEmergencyPhone() async {
+    var user = FirebaseAuth.instance.currentUser!;
+    var data = await getUserInfoWithUID(user.uid);
+
+    setState(() => { _emergencyPhone = data["emergency_phone"] });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadEmergencyPhone();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Icon(
+        children: [
+          const Icon(
             Icons.warning,
             size: 100,
           ),
+          const Center(
+            child: Text(
+              "Connect to emergency contacts",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
           Center(
             child: Text(
-              "Connect to emergency contacts"
+              _emergencyPhone,
+              style: const TextStyle(
+                fontSize: 18,
+              ),
             ),
           ),
         ],
